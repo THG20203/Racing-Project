32362780a0a2a2038e58d1fadb977996b64d22aa
THG20203 <tristanhgriffiths8@yahoo.com>
2024-04-13 00:34:36 +0100

    split screen compelted

--- js/about.js
+++ js/about.js
@@ -1,7 +1,9 @@
+/////// VARIABLES
 const left = document.querySelector(".left");
 const right = document.querySelector(".right");
 const splitScreen = document.querySelector(".split-screen");
 
+/////// EVENT LISTENERS
 /* when the person is hovering the left */
 left.addEventListener("mouseenter", () => {
   splitScreen.classList.add("hover-left");
