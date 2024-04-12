7e2cb491711ac314f86ca8a5d2f72af7434d6f1c
THG20203 <tristanhgriffiths8@yahoo.com>
2024-04-13 00:27:37 +0100

    beginning work on event listeners for split screen

--- js/about.js
+++ js/about.js
@@ -1,1 +1,13 @@
+const left = document.querySelector(".left");
+const right = document.querySelector(".right");
+const splitScreen = document.querySelector(".split-screen");
 
+/* when the person is hovering the left */
+left.addEventListener("mouseenter", () => {
+  splitScreen.classList.add("hover-left");
+});
+
+/* when the person is no longer hovering the left */
+left.addEventListener("mouseleave", () => {
+  splitScreen.classList.remove("hover-left");
+});
